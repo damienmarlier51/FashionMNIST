5e32c0088350bed11870118459908ece5db695d9
damienmarlier51 <damien.marlier@hotmail.fr>
2020-05-19 21:09:47 +0800

    Add click to call train from command line

--- src/train.py
+++ src/train.py
@@ -1,8 +1,8 @@
 from src.fns import model_fn, eval_input_fn, train_input_fn
-from pathlib import Path
 from tensorflow import keras
 import tensorflow as tf
 import shutil
+import click
 import os
 
 
@@ -12,20 +12,20 @@ def serving_input_receiver_fn():
     return tf.estimator.export.ServingInputReceiver(inputs, inputs)
 
 
-def train(model_dir,
-          batch_size=32,
-          epochs=10,
-          checkpoint_path=None,
-          reset=False):
+def train_(model_dir=None,
+           batch_size=32,
+           epochs=10,
+           checkpoint=None,
+           reset=False):
 
-    if os.path.exists(checkpoint_path) is False:
+    if checkpoint is not None and os.path.exists(checkpoint) is False:
         raise Exception("Checkpoint doesn't exist")
 
-    clear = True
-    if clear is True:
-        shutil.rmtree(model_dir)
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
+    if model_dir is not None:
+        if reset is True:
+            shutil.rmtree(model_dir)
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
 
     fashion_mnist = keras.datasets.fashion_mnist
     (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
@@ -43,7 +43,7 @@ def train(model_dir,
 
     estimator = tf.estimator.Estimator(model_fn=model_fn,
                                        config=training_config,
-                                       warm_start_from=checkpoint_path)
+                                       warm_start_from=checkpoint)
 
     train_spec = tf.estimator.TrainSpec(lambda: train_input_fn(train_x, train_y, batch_size, epochs),
                                         max_steps=train_steps)
@@ -58,18 +58,24 @@ def train(model_dir,
                                 serving_input_receiver_fn=serving_input_receiver_fn)
 
 
+@click.command()
+@click.option('--model_dir', default=None)
+@click.option('--checkpoint', default=None)
+@click.option('--batch_size', default=32)
+@click.option('--epochs', default=10)
+@click.option('--reset', default=False)
+def train(checkpoint,
+          model_dir,
+          batch_size,
+          epochs,
+          reset):
+
+    train_(model_dir=model_dir,
+           batch_size=batch_size,
+           epochs=epochs,
+           checkpoint=checkpoint,
+           reset=reset)
+
+
 if __name__ == "__main__":
-
-    file_path = os.path.abspath(__file__)
-    project_dir = Path(file_path).resolve().parents[1]
-    model_dir = "{}/model/v1".format(project_dir)
-
-    checkpoint_name = "model.ckpt-188"
-    checkpoint_path = "{}/{}".format(model_dir, checkpoint_name)
-
-    batch_size = 32
-    epochs = 10
-
-    train(model_dir=model_dir,
-          batch_size=batch_size,
-          epochs=epochs)
+    train()
